6e00f202c7d33ec32995473fb1213887e78c1a27
gnocarroll <gocarrol@purdue.edu>
2025-07-31 23:59:34 -0500

    think that I will define the classes that will be used to hold the parsed program structure then can have parser use them to do parsing

--- capsule/parser/node_types.py
+++ capsule/parser/node_types.py
@@ -1,12 +1,21 @@
-from typing import NamedTuple
-
-from .types import (
-    Terminal,
-)
-
-class CreateVar(NamedTuple):
-    
+from dataclasses import dataclass
 
 
-class FunctionDef(NamedTuple):
-    args = list[CreateVar]
+@dataclass
+class Program:
+    definitions: list["Definition"]
+
+
+@dataclass
+class Definition:
+    definition: "TypeDefinition | FunctionDefinition"
+
+
+@dataclass
+class TypeDefinition:
+    pass
+
+
+@dataclass
+class FunctionDefinition:
+    pass
